f77b3ea71d738465c6ce69716c49ee52a5ccf8af
jayrungta <Jay@Jays-MBP.fios-router.home>
2017-05-01 22:38:36 -0400

    Updated server.js

--- server.js
+++ server.js
@@ -6,39 +6,65 @@ const cookieParser = require('cookie-parser');
 const configRoutes = require("./routes");
 const app = express();
 const static = express.static(__dirname + '/public');
-
-const flightData = require("./data/flights");
-
+const session = require('express-session')
+const passport = require('passport')
+var flash = require("connect-flash");
+app.use(flash());
+const exphbs = require('express-handlebars');
+const Handlebars = require('handlebars');
 // //wkhtmlpdf
 // const wkhtmlpdf = require('wkhtmlpdf');
 // const fs = require('fs');
-//
+
 // // URL      get the pdf file
 // wkhtmltopdf('http://localhost:3000/recipes', { pageSize: 'letter' })
 //   .pipe(fs.createWriteStream('out.pdf'));
+
+const handlebarsInstance = exphbs.create({
+    defaultLayout: 'main',
+    // Specify helpers which are only registered on this instance.
+    helpers: {
+        asJSON: (obj, spacing) => {
+            if (typeof spacing === "number")
+                return new Handlebars.SafeString(JSON.stringify(obj, null, spacing));
+
+            return new Handlebars.SafeString(JSON.stringify(obj));
+        }
+    },
+    partialsDir: [
+        'views/partials/'
+    ]
+});
+
+const rewriteUnsupportedBrowserMethods = (req, res, next) => {
+    // If the user posts to the server with a property called _method, rewrite the request's method
+    // To be that method; so if they post _method=PUT you can now allow browsers to POST to a route that gets
+    // rewritten in this middleware to a PUT route
+    if (req.body && req.body._method) {
+        req.method = req.body._method;
+        delete req.body._method;
+    }
+
+    // let the next middleware run:
+    next();
+};
 
 app.use("/public", static);
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.engine('handlebars', handlebarsInstance.engine);
+app.set('view engine', 'handlebars');
+
+//Passport
+app.use(session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 configRoutes(app);
 
 app.listen(3000, () => {
     console.log("We've now got a server!");
     console.log("Your routes will be running on http://localhost:3000");
-
-    // Get all airlines info
-    // const aviationJson = require("aviation-json");
-    // let airlines = aviationJson.airlines;
-    // console.log(airlines);
-
-    //Search flights
-    // flightData.searchFlights("1", "USD5000", "20", "NYC", "LAX", "2017-12-14").then((flights) => {
-    //     console.log('done');
-    //     console.log(flights);
-    // }).catch(function(error) {
-    //     console.log(error);
-    // });
-
 });
