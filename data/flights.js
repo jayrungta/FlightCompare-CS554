53aec6d9b0cfe19f246b81b7e74d74b6f15e4ab1
jayrungta <Jay@Jays-MBP.fios-router.home>
2017-05-04 18:31:26 -0400

    Routes for flight search

--- data/flights.js
+++ data/flights.js
@@ -1,46 +1,51 @@
 const request = require('request');
-const endPoint = "https://www.googleapis.com/qpxExpress/v1/trips/search?key="+"AIzaSyCFSW0qP1DVtrvwGDvjeDeDJeHsMCJs1pA";
+const endPoint = "https://www.googleapis.com/qpxExpress/v1/trips/search?key=" + "AIzaSyCFSW0qP1DVtrvwGDvjeDeDJeHsMCJs1pA";
 
 module.exports = {
     /**
      * @returns flights - Returns a list of flights.
      * @throws Will throw an error if request has an error or no flights found.
      */
-    searchFlights(adultCount, maxPrice, solutions, origin, destination, date){
+    searchFlights(adultCount, maxPrice, solutions, origin, destination, date) {
         return new Promise((resolve, reject) => {
             let data = {
                 "request": {
-                  "passengers": {
-                    "adultCount": adultCount
-                  },
-                  "maxPrice": maxPrice,
-                  "solutions": solutions,
-                "slice": [
-                      {
-                        "origin": origin,
-                        "destination": destination,
-                        "date": date
-                      }
+                    "passengers": {
+                        "adultCount": adultCount
+                    },
+                    "maxPrice": maxPrice,
+                    "solutions": solutions,
+                    "slice": [
+                        {
+                            "origin": origin,
+                            "destination": destination,
+                            "date": date,
+                            "maxStops": 0
+                        }
                     ]
                 }
             };
 
-            request({method: "post",  url: endPoint,  body: data,  json: true}, (err, resp, body) => {
+            request({ method: "post", url: endPoint, body: data, json: true }, (err, resp, body) => {
                 let flights = [];
                 let jsonObject = {};
                 let airline;
                 let price;
-
-                if (body.error){
+                let departureTime, arrivalTime, origin, destination;
+                if (body.error) {
                     console.error(body.error);
                     reject(body.error);
                 }
 
                 try {
-                    for(i=0; i < body.trips.tripOption.length; i++){
-                        airline = body.trips.tripOption[i].slice[0].segment[0].flight.carrier
-                        price = body.trips.tripOption[i].saleTotal
-                        jsonObject = {"airline": airline , "price": price};
+                    for (i = 0; i < body.trips.tripOption.length; i++) {
+                        airline = body.trips.tripOption[i].slice[0].segment[0].flight.carrier;
+                        price = body.trips.tripOption[i].saleTotal;
+                        arrivalTime = body.trips.tripOption[i].slice[0].segment[0].leg[0].arrivalTime;
+                        departureTime = body.trips.tripOption[i].slice[0].segment[0].leg[0].departureTime;
+                        origin = body.trips.tripOption[i].slice[0].segment[0].leg[0].origin;
+                        destination = body.trips.tripOption[i].slice[0].segment[0].leg[0].destination;
+                        jsonObject = { "airline": airline, "price": price, "arrivalTime":arrivalTime, "departureTime":departureTime, "origin":origin, "destination": destination };
                         flights.push(jsonObject);
                     }
                 } catch (e) {
@@ -52,7 +57,6 @@ module.exports = {
                 } else {
                     reject('No flights!');
                 }
-
             });
         });
     },
--- routes/search.js
+++ routes/search.js
@@ -2,12 +2,47 @@ const express = require('express');
 const router = express.Router();
 const data = require("../data");
 const userData = data.users;
+const flightData = data.flights;
+const aviationJson = require("aviation-json");
+
+let airlineInfo = null;
+
+if(!airlineInfo){
+    // Get all airlines info
+    airlineInfo = aviationJson.airlines;
+    console.log(airlineInfo);
+}
 
 router.get("/", (req, res) => {
     if(req.user)
-    res.render("search",{})
+    res.render("search",{});
     else
-    res.redirect("/login")
+    res.redirect("/login");
 });
+
+router.post('/', async (req,res)=>{
+    let adultCount = req.body.adultCount;
+    let maxPrice = req.body.maxPrice;
+    let solutions = "20";
+    let origin = req.body.origin;
+    let destination = req.body.destination;
+    let date = req.body.date;
+
+    try{
+    let flights = await flightData.searchFlights(adultCount, maxPrice, solutions, origin, destination, date);
+    res.json(flights);
+    }
+    catch(err){
+    res.status(500).send(err);
+    }
+});
+
+//  // Search flights
+//     flightData.searchFlights("1", "USD5000", "20", "NYC", "LAX", "2017-12-14").then((flights) => {
+//         console.log('done');
+//         console.log(flights);
+//     }).catch(function(error) {
+//         console.log(error);
+//     });
 
 module.exports = router;
